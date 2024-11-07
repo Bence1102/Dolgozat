66375432bc90c66ff104b40aee461edfd98be889
Bence1102 <szente.bence04@gmail.com>
2024-11-07 09:01:00 +0100

    masodik feladat

--- feladatok.py
+++ feladatok.py
@@ -1,3 +1,4 @@
+import random
 def paros_szamok():
     paros:int=int(input("Adj meg egy páros számot:"))
     while paros%2!=0:
@@ -6,4 +7,24 @@ def paros_szamok():
         print("Ez nem páros! Adj meg egy páros számot!")
     return paros
 
-def 
+def harommal_oszt():
+    lista=[]
+    i=0
+    while i<13:
+        szam:int=int(random.random()*(141)+10)
+        lista.append(szam)
+        i+=1
+    return lista
+
+def oszt_3mal(lista):
+    i:int=0
+    db:int=0
+    while(i<len(lista)):
+        if(lista[i]%3==0):
+            db+=1
+        i+=1  
+    return  db
+
+
+fef
+    
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import feladatok
 
 
-paros=feladatok.paros_szamok()
-print(paros)
+elso_feladat=feladatok.paros_szamok()
+print(elso_feladat)
+
+lista=feladatok.harommal_oszt()
+print(lista)
+
+masodik_fel2resz=feladatok.oszt_3mal(lista)
+print(f"A számok között {masodik_fel2resz} db 3-mal osztható van!")
