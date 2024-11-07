60be9828ee32778533ce11e4e9d59ec674b0029e
Bence1102 <szente.bence04@gmail.com>
2024-11-07 09:49:40 +0100

    3-as is kész

--- feladatok.py
+++ feladatok.py
@@ -26,5 +26,49 @@ def oszt_3mal(lista):
     return  db
 
 
-fef
+def fel3(text,N):
+    i:int=0
+    hossz:int=0
+    if (N>len(text)):
+        print("Nincs N karakter!")
+    else:
+        print(text[N-1].upper()*3)
+        
+
+def fel4():
+    szov=[]
+    s=szov
+    szoveg:str=""
+    while szoveg !="@":
+        szoveg=input("Adj meg egy nevet!(Kilépés a @-jellel!)")
+        szov.append(szoveg)
+        if szoveg!="@":
+            print(szoveg)
+
+def fel5():
+
+    
+    jatek=[]
+    i:int =0
+    while (i<3):
+        erem:int=int(random.random()*2+1)
+        if (erem==1):
+            jatek.append("Kő")
+        elif (erem==2):
+            jatek.append("Paprír")
+        i+=1
+        else (erem==3):
+        jatek.append("Paprír")
+        i+=1
+    return jatek
+
+
+
+
+
+
+
+
+        
+    
     
--- main.py
+++ main.py
@@ -9,3 +9,11 @@ print(lista)
 
 masodik_fel2resz=feladatok.oszt_3mal(lista)
 print(f"A számok között {masodik_fel2resz} db 3-mal osztható van!")
+
+print("Hármas feladat")
+harmadik=feladatok.fel3()
+print(f"A szöveg 4. karaktere {harmadik}")
+
+feladat4=feladatok.fel4()
+print(f"A felhasználó {feladat4} nevet adott meg!")
+
